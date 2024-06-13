1f10a07a18574cde6d695243e4f9a0fb69a486e2
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-06-13 11:45:45 +0300

    v2

--- src/App.java
+++ src/App.java
@@ -1,12 +1,6 @@
 import business.UserManager;
-import core.Db;
 import core.Helper;
-import entity.User;
 import view.AdminView;
-import view.LoginView;
-
-import javax.swing.*;
-import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) {
@@ -14,6 +8,5 @@ public class App {
         //LoginView loginView = new LoginView();
         UserManager userManager = new UserManager();
         AdminView adminView = new AdminView(userManager.findByLogin("admin", "12345"));
-        //loginView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
