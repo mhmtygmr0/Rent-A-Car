590dce866f032abbab944d087ceea335f67ab589
Mehmet Yağmur <mehmetygmrr02@gmail.com>
2024-06-21 00:13:03 +0300

    v8

--- src/App.java
+++ src/App.java
@@ -1,12 +1,11 @@
-import business.UserManager;
 import core.Helper;
-import view.AdminView;
+import view.LoginView;
 
 public class App {
     public static void main(String[] args) {
         Helper.setTheme();
-        //LoginView loginView = new LoginView();
-        UserManager userManager = new UserManager();
-        AdminView adminView = new AdminView(userManager.findByLogin("admin", "12345"));
+        LoginView loginView = new LoginView();
+        //admin
+        //12345
     }
 }
